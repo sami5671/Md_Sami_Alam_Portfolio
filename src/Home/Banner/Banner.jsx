26fcb7f8530204874a85259bf3e71fac3a38c4f8
sami5671 <samialam5671@gmail.com>
2024-03-06 14:57:37 +0600

    Completed the banner section with responsive design

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -6,6 +6,13 @@ import react from "../../assets/images/react.png";
 import nodejs from "../../assets/images/node.jpg";
 import profile from "../../assets/images/profile2.png";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaFileDownload } from "react-icons/fa";
+import { MdOutlineMarkEmailRead } from "react-icons/md";
+
 const Banner = () => {
   // ----------------------------------------------------------------
   const [text] = useTypewriter({
@@ -18,21 +25,80 @@ const Banner = () => {
   // ----------------------------------------------------------------
   return (
     <>
+      {/* for small devices  */}
+      <div className="block lg:hidden flex justify-center mt-4">
+        <img src={profile} className="rounded-full w-[200px]" alt="" />
+      </div>
+      {/* for small devices  */}
+
       <div className="flex justify-center lg:justify-start">
         {/* left */}
-        <div className="lg:mt-44 lg:ml-24">
-          <h1 className="text-5xl leading-snug text-slate-700 font-semibold font-serif">
+        <div className="mt-12 lg:mt-44 lg:ml-24">
+          <h1 className="text-2xl lg:text-5xl lg:leading-snug text-slate-700 font-semibold font-serif">
             Hi, There.... <br />I Am Md Sami Alam
           </h1>
-          <p className="mt-4 text-3xl ">
+          <p className="mt-4 text-[18px] lg:text-3xl text-pink-500">
             {text}
             <Cursor cursorBlinking="false" cursorStyle="|" />
           </p>
+
+          {/* social links */}
+          <div className="flex mt-2 lg:mt-8 gap-2">
+            <div className="flex items-center justify-center">
+              <div className="flex space-x-2">
+                <a
+                  href="https://github.com/sami5671"
+                  className="text-slate-800 hover:text-pink-300 rounded-full glow p-2"
+                >
+                  <FaGithub className="text-[28px]" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/sami5671/"
+                  className="text-slate-800 hover:text-pink-300 rounded-full glow p-2"
+                >
+                  <FaLinkedin className="text-[28px]" />
+                </a>
+                <a
+                  href="https://www.instagram.com/samialam5671/"
+                  className="text-slate-800 hover:text-pink-300 rounded-full glow p-2"
+                >
+                  <FaInstagramSquare className="text-[28px]" />
+                </a>
+                <a
+                  href="https://www.facebook.com/samialam5559"
+                  className="text-slate-800 hover:text-pink-300 rounded-full glow p-2"
+                >
+                  <FaFacebook className="text-[28px]" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* social links */}
+          {/* button resume and contact Me */}
+          <div className="flex gap-6 mt-6 mb-44">
+            <div>
+              <button className=" bg-pink-500 text-white px-2 py-1 rounded-md transition duration-300 ease-in-out hover:bg-pink-300 hover:text-black font-semibold">
+                <span className="flex gap-1 items-center text-[16px]">
+                  Download Resume <FaFileDownload />
+                </span>
+              </button>
+            </div>
+
+            <div>
+              <button className=" bg-pink-200 px-2 py-1 rounded-md transition duration-300 ease-in-out hover:bg-pink-400 hover:text-white font-semibold">
+                <span className="flex gap-1 items-center text-[16px]">
+                  Contact Me <MdOutlineMarkEmailRead />
+                </span>
+              </button>
+            </div>
+          </div>
+          {/* button resume and contact Me */}
         </div>
 
         {/* right */}
         <div>
-          <div className="lol h-[150px] w-[150px] lg:h-[400px] lg:w-[400px] lg:mr-32">
+          <div className="lol hidden lg:block h-[150px] w-[150px] lg:h-[400px] lg:w-[400px] lg:mr-32">
             <div className="js">
               <img src={js} width="50px" height="50px" alt="" />
             </div>
