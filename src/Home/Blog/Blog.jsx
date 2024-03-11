fb9e551d6cfbfd6e7bf877a16f840b4dfcfa860e
sami5671 <samialam5671@gmail.com>
2024-03-11 23:12:29 +0600

    Added the card design and map through the card

--- src/Home/Blog/Blog.jsx
+++ src/Home/Blog/Blog.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import BlogCard from "./BlogCard";
 const Blog = () => {
+  // ----------------------------------------------------------------
+
+  const [blog, setBlog] = useState(0);
+
+  useEffect(() => {
+    fetch("../../../public/Blog.json")
+      .then((res) => res.json())
+      .then((data) => setBlog(data));
+  }, []);
+
+  console.log(blog);
+  // ----------------------------------------------------------------
   return (
-    <div>
-      <BlogCard></BlogCard>
-    </div>
+    <>
+      <section className="px-12">
+        <div className="text-2xl px-2 lg:text-4xl mb-12 font-mono font-bold border-b-2  bg-gradient-to-tr from-pink-500 to-lime-700 text-transparent bg-clip-text ">
+          <h1>My Blog</h1>
+        </div>
+      </section>
+
+      <section className="lg:px-24 py-1">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+          {blog.map((item) => (
+            <BlogCard key={item.id} blog={blog}></BlogCard>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/Home/Blog/BlogCard.jsx
+++ src/Home/Blog/BlogCard.jsx
@@ -1,7 +1,24 @@
-const BlogCard = () => {
+const BlogCard = ({ blog }) => {
+  console.log(blog);
   return (
     <div>
-      <h1>card</h1>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            Shoes! <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
